8cecbae1d2e312149862f53ec03c98b76ce7ca0b
guoj <guoj@landicorp.com>
2021-06-06 09:49:18 +0800

    为recycleView添加滑动删除效果 添加snackebar提示撤销 使用coordinatorlayout布局snackbar弹出时候可以向上伸缩 删除和撤销动作不在滚动列表反馈

--- app/src/main/java/com/guoj/worddemo/fragment/WordFragment.java
+++ app/src/main/java/com/guoj/worddemo/fragment/WordFragment.java
@@ -20,10 +20,12 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.guoj.worddemo.R;
 import com.guoj.worddemo.Word;
 import com.guoj.worddemo.WordViewModel;
@@ -38,6 +40,8 @@ public class WordFragment extends Fragment {
     RecyclerView recyclerView;
     WordAdpter wordAdpter;
     LiveData<List<Word>> filteredWords;
+    List<Word> allWords;
+    boolean undo;
     public WordFragment() {
         //设置显示menu
         setHasOptionsMenu(true);
@@ -45,23 +49,22 @@ public class WordFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_word,container,false);
+        return inflater.inflate(R.layout.fragment_word, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.i("guoj","======onViewCreated");
+        Log.i("guoj", "======onViewCreated");
     }
-
 
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Log.i("guoj","======onActivityCreated");
+        Log.i("guoj", "======onActivityCreated");
         wordViewModel = new ViewModelProvider(requireActivity()).get(WordViewModel.class);
         initView();
     }
@@ -72,10 +75,11 @@ public class WordFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         wordAdpter = new WordAdpter(wordViewModel);
         recyclerView.setAdapter(wordAdpter);
-        recyclerView.setItemAnimator(new DefaultItemAnimator(){
+        recyclerView.setItemAnimator(new DefaultItemAnimator() {
             @Override
-            public void onAnimationFinished(@NonNull  RecyclerView.ViewHolder viewHolder) {
+            public void onAnimationFinished(@NonNull RecyclerView.ViewHolder viewHolder) {
                 super.onAnimationFinished(viewHolder);
+                //动画结束后更新item的序号显示
                 LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
                 if (layoutManager != null) {
                     int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
@@ -83,43 +87,66 @@ public class WordFragment extends Fragment {
                     for (int i = firstVisibleItemPosition; i <= lastVisibleItemPosition; i++) {
                         WordAdpter.WordViewHolder holder = (WordAdpter.WordViewHolder) recyclerView.findViewHolderForAdapterPosition(i);
                         if (holder != null) {
-                            holder.tv_num.setText(String.valueOf(i+1));
+                            holder.tv_num.setText(String.valueOf(i + 1));
                         }
                     }
                 }
             }
         });
-        filteredWords=wordViewModel.getAllWords();
+        filteredWords = wordViewModel.getAllWords();
         filteredWords.observe(getViewLifecycleOwner(), words -> {
+            allWords = words;
             if (wordAdpter.getItemCount() != words.size()) {
+                //删除数据时候不滑动
+                if (wordAdpter.getItemCount() < words.size()&&!undo) {
+                    recyclerView.smoothScrollBy(0, -200);
+                }
+                undo=false;
                 wordAdpter.submitList(words);
-                recyclerView.smoothScrollBy(0,-200);
-//                recyclerView.scrollToPosition(wordAdpter.getItemCount()-1);
             }
         });
         FloatingActionButton floatingActionButton = requireActivity().findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(view -> {
-            NavController controller= Navigation.findNavController(view);
+            NavController controller = Navigation.findNavController(view);
             controller.navigate(R.id.action_wordFragment_to_addFragment);
         });
+        //为recycleView添加滑动和拖拽处理
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.START | ItemTouchHelper.END) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                //定义一个list保存words 防止使用livedate在后台获取数据时候出现空指针
+                Word wordtoDelete = allWords.get(viewHolder.getAdapterPosition());
+                wordViewModel.deleteWords(wordtoDelete);
+                Snackbar.make(requireActivity().findViewById(R.id.wordFragmentLayout), "删除了一条数据", Snackbar.LENGTH_SHORT)
+                        .setAction("撤销", v -> {
+                            undo=true;
+                            wordViewModel.insertWords(wordtoDelete);
+                        }).show();
+            }
+        }).attachToRecyclerView(recyclerView);
     }
 
     @Override
     public void onResume() {
         InputMethodManager imm = (InputMethodManager) getSystemService(requireContext(), InputMethodManager.class);
         if (imm != null) {
-            imm.hideSoftInputFromWindow(getView().getWindowToken(),0);
+            imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
         }
         super.onResume();
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull  Menu menu, @NonNull MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_word,menu);
+        inflater.inflate(R.menu.menu_word, menu);
 //       xml中的menuitem需要设置 app:actionViewClass="android.widget.SearchView" 否则会空指针
         MenuItem menuItem = menu.findItem(R.id.app_bar_search);
-        SearchView searchView= (SearchView) menuItem.getActionView();
+        SearchView searchView = (SearchView) menuItem.getActionView();
         searchView.setMaxWidth(700);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -131,31 +158,36 @@ public class WordFragment extends Fragment {
             public boolean onQueryTextChange(String newText) {
                 //移除之前设置的观察，防止碰撞
                 filteredWords.removeObservers(getViewLifecycleOwner());
-                filteredWords=wordViewModel.getWordsWithPatten(newText);
+                filteredWords = wordViewModel.getWordsWithPatten(newText);
                 filteredWords.observe(getViewLifecycleOwner(), words -> {
+                    allWords = words;
                     if (wordAdpter.getItemCount() != words.size()) {
-                       wordAdpter.submitList(words);
-                       recyclerView.smoothScrollBy(0,-200);
-//                       recyclerView.scrollToPosition(wordAdpter.getItemCount()-1);
+                        if (wordAdpter.getItemCount() < words.size()&&!undo) {
+                            recyclerView.smoothScrollBy(0, -200);
+                        }
+                        undo=false;
+                        wordAdpter.submitList(words);
                     }
                 });
                 return true;
             }
         });
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if (R.id.menu_add==item.getItemId()){
-            Word word1=new Word("Hello","你好");
-            Word word2=new Word("World","世界");
-            Word word3=new Word("blue","蓝色");
-            wordViewModel.insertWords(word1,word2,word3);
-        }else if (R.id.menu_clear==item.getItemId()){
-            AlertDialog.Builder builder=new AlertDialog.Builder(requireActivity());
+        if (R.id.menu_add == item.getItemId()) {
+            Word word1 = new Word("Hello", "你好");
+            Word word2 = new Word("World", "世界");
+            Word word3 = new Word("blue", "蓝色");
+            wordViewModel.insertWords(word1, word2, word3);
+        } else if (R.id.menu_clear == item.getItemId()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
             builder.setMessage("清空数据?")
                     .setPositiveButton("确定", (dialog, which) -> wordViewModel.deleteAllWords())
-                    .setNegativeButton("取消", (dialog, which) -> { })
+                    .setNegativeButton("取消", (dialog, which) -> {
+                    })
                     .create()
                     .show();
         }
